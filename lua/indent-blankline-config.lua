6bf6f559a1e30916ad7454c225da9524199c944b
Billy Arante <arantebillywilson@gmail.com>
2023-04-10 18:40:42 +0800

    fix(indent-blankline): 🐛 adjust of blanklines before a comment

--- lua/indent-blankline-config.lua
+++ lua/indent-blankline-config.lua
@@ -1,5 +1,6 @@
 vim.opt.termguicolors = true
 vim.cmd [[highlight IndentBlanklineChar guifg=#586E75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineSpaceChar guifg=#586E75 gui=nocombine]]
 
 vim.g.indent_blankline_char = "┊"
 vim.g.indent_blankline_char_blankline = "┊"
@@ -7,5 +8,6 @@ vim.g.indent_blankline_char_blankline = "┊"
 require("indent_blankline").setup {
   char_highlight_list = {
     "IndentBlanklineChar",
+    "IndentBlanklineSpaceChar",
   }
 }
