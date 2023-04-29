d6edfab0b7e250231ad3bf2b23f646e1da8620dd
Billy Arante <arantebillywilson@gmail.com>
2023-04-29 22:51:01 +0800

    feat(c): ✨ add c language file type options

---
+++ ftplugin/c.lua
@@ -0,0 +1,3 @@
+vim.opt.shiftwidth = 8
+vim.opt.softtabstop = 8
+vim.opt.tabstop = 8
--- lua/options.lua
+++ lua/options.lua
@@ -27,3 +27,5 @@ opt.tabstop = 2
 -- [[ splits ]]
 opt.splitright = true
 opt.splitbelow = true
+
+opt.swapfile = false
