da6c3c8988fc7e97c95d7e716a46199487582962
Billy Arante <arantebillywilson@gmail.com>
2023-11-25 16:55:58 +0800

    refactor(indent-blankline): ♻️ reconfigure for version 3

--- lua/indent-blankline-config.lua
+++ lua/indent-blankline-config.lua
@@ -1,13 +1,8 @@
-vim.opt.termguicolors = true
-vim.cmd [[highlight IndentBlanklineChar guifg=#586E75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineSpaceChar guifg=#586E75 gui=nocombine]]
-
-vim.g.indent_blankline_char = "┊"
-vim.g.indent_blankline_char_blankline = "┊"
-
-require("indent_blankline").setup {
-  char_highlight_list = {
-    "IndentBlanklineChar",
-    "IndentBlanklineSpaceChar",
+require("ibl").setup {
+  indent = {
+    char = "┊"
+  },
+  scope = {
+    enabled = false
   }
 }
