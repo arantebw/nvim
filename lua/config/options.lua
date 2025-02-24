40e337228d5bc52503b7e92d97ff40522beb1ec8
Billy Arante <arantebillywilson@gmail.com>
2025-02-24 12:01:05 +0800

    feat(options): ✨ setup pyright and ruff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+local g = vim.g
 
+-- general
 opt.colorcolumn = "80"
+
+-- python
+g.lazyvim_python_lsp = "pyright"
+g.lazyvim_python_ruff = "ruff"
